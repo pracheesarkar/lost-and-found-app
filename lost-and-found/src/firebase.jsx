3fd7684634d1d8080cf2971a54076449cf009393
pracheesarkar <117396817+pracheesarkar@users.noreply.github.com>
2024-10-28 18:14:32 -0400

    Create firebase.jsx

---
+++ lost-and-found/src/firebase.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+        apiKey: "AIzaSyDgo0o4UbeFx3Ze6zFp1wxIdQMzkTceGaQ",
+        authDomain: "lost-and-found-74e81.firebaseapp.com",
+        projectId: "lost-and-found-74e81",
+        storageBucket: "lost-and-found-74e81.appspot.com",
+        messagingSenderId: "612440360072",
+        appId: "1:612440360072:web:2fbdef412279e19b0d4b3d",
+        measurementId: "G-CR7X7GJ23V"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+export{ firebaseApp, db };
